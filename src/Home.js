30c4e3ce38dabd0fa9fc0021a8cd536513b1e041
Ashwin Aravindan <aaravin@fb.com>
2019-03-11 14:43:48 -0700

    Test custom

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './index.css';
 
 class Home extends Component {
+  componentDidMount() {
+    window.fbq('trackCustom', 'Test1', {
+      value: 10.00
+    });
+  }
+
   render() {
     return (
       <div id='content'>Home</div>
--- src/Users.js
+++ src/Users.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './index.css';
 
 class Users extends Component {
+  componentDidMount() {
+    window.fbq('trackCustom', 'Test1', {
+      value: 3.00
+    });
+  }
+
   render() {
     return (
       <div id='content'>Users</div>
